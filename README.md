074c950f5a381f2f25461377a4059aa2d23a2478
xiongjiapeng <xiongjiapeng@bytedance.com>
2022-03-26 17:08:30 +0800

    first commit

---
+++ README.md
@@ -0,0 +1,112 @@
+# koa-grpc
+
+[![NPM Version][npm-image]][npm-url]
+[![Node Version][node-image]][node-url]
+
+The thrift middle of Koa.
+
+## Installation
+
+```bash
+npm i koa-grpc -S
+```
+
+## Usage
+
+- simple app
+
+```typescript
+var UnpkgService = require('./gen-nodejs/UnpkgService');
+import { Context } from 'koa';
+import KoaThrift from 'koa-grpc';
+
+const app = new KoaThrift({ service: UnpkgService });
+
+// not found
+app.use((ctx: Context) => {
+  console.log('ctx.request: ', ctx.request);
+});
+
+// result -> InvalidOperation TApplicationException: Not Found
+
+app.listen(9090);
+console.log('listening on 9090...');
+```
+
+- with route
+
+```typescript
+var UnpkgService = require('./gen-nodejs/UnpkgService');
+const mount = require('koa-mount');
+import { Context } from 'koa';
+import KoaThrift from 'koa-grpc';
+
+const app = new KoaThrift({ service: UnpkgService });
+
+// with route
+app.use(
+  mount('/Publish', function (ctx: Context) {
+    console.log('ctx.request: ', ctx.request);
+    ctx.body = { code: 0, message: 'publish success' };
+  })
+);
+
+app.listen(9090);
+console.log('listening on 9090...');
+```
+
+- use middleware
+
+```typescript
+var UnpkgService = require('./gen-nodejs/UnpkgService');
+const mount = require('koa-mount');
+import { Context } from 'koa';
+import KoaThrift from 'koa-grpc';
+
+const app = new KoaThrift({ service: UnpkgService });
+
+// use middleware
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  console.log( `process ${ctx.path} request from ${ctx.ip} cost ${Date.now() - start}ms`);
+});
+
+// with route
+app.use(
+  mount('/Publish', async (ctx: Context) => {
+    console.log('ctx.request: ', ctx.request);
+    await sleep(300);
+    ctx.body = { code: 0, message: 'publish success' };
+  })
+);
+
+app.listen(9090);
+console.log('listening on 9090...');
+
+function sleep(delay = 1000) {
+  return new Promise((resolve) => setTimeout(resolve, delay));
+}
+```
+
+## Examples
+
+examples with client are listed at [examples](https://github.com/cooperhsiung/koa-grpc/tree/master/examples)
+
+## Todo
+
+- [ ] middleware
+- [ ] wrap client
+
+## Others
+
+[Thrift Missing Guide](https://diwakergupta.github.io/thrift-missing-guide)
+
+## License
+
+MIT
+
+[npm-image]: https://img.shields.io/npm/v/koa-grpc.svg
+[npm-url]: https://www.npmjs.com/package/koa-grpc
+[node-image]: https://img.shields.io/badge/node.js-%3E=8-brightgreen.svg
+[node-url]: https://nodejs.org/download/
---
+++ index.ts
@@ -0,0 +1,95 @@
+/**
+ * Created by Cooper on 2022/03/26.
+ */
+import { sendUnaryData, ServiceDefinition, UntypedServiceImplementation } from '@grpc/grpc-js';
+import Koa, { Context } from 'koa';
+import compose from 'koa-compose';
+
+const grpc = require('@grpc/grpc-js');
+const context = require('koa/lib/context');
+
+type Options = {
+  env?: string;
+  keys?: string[];
+  proxy?: boolean;
+  subdomainOffset?: number;
+  proxyIpHeader?: string;
+  maxIpsCount?: number;
+};
+
+class Application extends Koa {
+  service: ServiceDefinition;
+
+  constructor(options: Options & { service: ServiceDefinition }) {
+    super(options);
+    this.service = options.service;
+    this.context = Object.create(context);
+  }
+
+  // @ts-ignore
+  createContext(call: any, method: any) {
+    const context = {
+      path: '/' + method,
+      originalUrl: '/' + method,
+    } as Context;
+    const request = (context.request = Object.create(this.request));
+    const response = (context.response = Object.create(this.response));
+    context.app = request.app = response.app = this as any;
+    context.req = request.req = response.req = { socket: call.call.stream.session.socket } as any;
+    context.res = request.res = response.res = { socket: call.call.stream.session.socket } as any;
+    request.ctx = response.ctx = context;
+    request.response = response;
+    response.request = request;
+    // context.originalUrl = request.originalUrl = req.url;
+    context.state = {};
+    return context;
+  }
+
+  // @ts-ignore
+  callback() {
+    const fn = compose(this.middleware);
+    return Object.keys(this.service).reduce((impl, method) => {
+      impl[method] = this.handleRequest(fn, method) as any;
+      return impl;
+    }, {} as UntypedServiceImplementation);
+  }
+
+  handleRequest(fnMiddleware: any, method: string) {
+    return async (call: any, callback: sendUnaryData<any>) => {
+      const ctx = this.createContext(call, method);
+      ctx.request = call.request;
+      ctx.socket = (call as any).call.stream.session.socket;
+      ctx.ip = ctx.socket.remoteAddress || '';
+      ctx.call = call;
+      Object.defineProperty(ctx, 'body', {
+        set(v: any) {
+          this.response = this._body = v;
+        },
+        get() {
+          return this._body;
+        },
+      });
+
+      try {
+        await fnMiddleware(ctx);
+        if (ctx.body === undefined) {
+          return callback({ code: grpc.status.UNIMPLEMENTED, details: 'Not Found' }, null);
+        }
+        callback(null, ctx.body);
+      } catch (err) {
+        callback(err, null);
+      }
+    };
+  }
+
+  // @ts-ignore
+  listen(...args: any) {
+    const server = new grpc.Server();
+    server.addService(this.service, this.callback());
+    return server.bindAsync(...args, grpc.ServerCredentials.createInsecure(), () => {
+      server.start();
+    });
+  }
+}
+
+export default Application;
